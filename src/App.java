e1db59a1f10ace2ef3275f1af49b3c9b5ea17885
Yollanda Lima <yollandalima123@gmail.com>
2023-09-08 23:24:56 -0300

    Atualizado

--- src/App.java
+++ src/App.java
@@ -1,24 +1,33 @@
 import java.util.Scanner;
 
 public class App {
-
     public static void main(String[] args) {
+        // Cria um objeto Scanner para leitura de entrada do teclado
         Scanner scanner = new Scanner(System.in);
 
+        // Solicita ao usuário que insira seu nome
         System.out.print("Digite seu nome: ");
         String nome = scanner.nextLine();
 
+        // Solicita ao usuário que insira sua idade
         System.out.print("Digite sua idade: ");
         int idade = scanner.nextInt();
 
+        // Fecha o Scanner para liberar recursos
         scanner.close();
 
+        // Imprime uma linha em branco para melhorar a legibilidade do código
         System.out.println();
 
+        // Exibe uma mensagem de boas-vindas com o nome do usuário e a disciplina
         System.out.println("Olá, " + nome + "!");
         System.out.println("Bem-vindo à disciplina de Engenharia Econômica para Software.");
 
-        // Substituímos o loop for por um bloco if para verificar a idade e exibir mensagens diferentes
+        String resposta = scanner.nextLine();
+
+        System.out.println(resposta);
+
+        // Verifica a idade do usuário e exibe uma mensagem apropriada
         if (idade < 18) {
             System.out.println("Você é menor de idade.");
         } else if (idade >= 18 && idade < 60) {
